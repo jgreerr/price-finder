f8edac232d65e1e0bd28086ffa2ce26bb78ccaa2
Jackson <jackjg2@gmail.com>
2022-06-30 13:13:19 -0400

    added correct element detection

--- background.js
+++ background.js
@@ -1,17 +1,18 @@
 
-chrome.contextMenus.onClicked.addListener((OnClickData) => {
-    console.log("Clicked");
-});
-
 chrome.runtime.onMessage.addListener((message) => {
     if (message == "create_menu") {
-        chrome.contextMenus.create({
-            title : 'Check price',
-            contexts : ["all"],
-            id : "1",
-            type : 'normal', 
-        })
-    } else if (message == "delete_menu") {
+        console.log("Message has been recieved");
+        chrome.contextMenus.removeAll(() => {
+            chrome.contextMenus.create({
+                title : 'Check price', 
+                contexts : ["all"], 
+                id : "1", 
+                type : 'normal'
+            })
+        });
+    } else if (message == "remove_menu") {
         chrome.contextMenus.removeAll();
     }
 })
+
+chrome.tabs.onActivated.addListener(() => chrome.contextMenus.removeAll());
--- content-script.js
+++ content-script.js
@@ -1,18 +1,61 @@
 
-// let script = document.createElement('script');
-// script.textContent = code;
-// document.head.appendChild(script); 
-// script.remove();
+// This script is injected into all websites that we want to compare prices with.
 
-document.addEventListener('mousedown', (event) => {
-    let selection = window.getSelection();
-    let code = console.log(selection.toString());
-    let script = document.createElement('script');
-    script.textContent = code; 
-    document.head.appendChild(script);
-    // if (selection.toString() == "Help") {
-    //     chrome.runtime.sendMessage({request: "create_menu"});
-    // } else {
-    //     chrome.runtime.sendMessage({request: "delete_menu"});
-    // }
-})
+const validClassNames = [
+    // Ebay
+   "s-item s-item__pl-on-bottom s-item--watch-at-corner",
+    // Amazon
+   "s-card-container s-overflow-hidden aok-relative s-expand-height "
+   + "s-include-content-margin s-latency-cf-section s-card-border",
+   // BestBuy
+   "sku-item", "shop-sponsored-listing"
+
+];
+const validIdNames = [
+    // Ebay
+    "CenterPanelInternal",
+    // Amazon
+    "ppd" 
+];
+
+//--- Determine if the context menu is appopriate for the section. 
+
+function addMouseDownForListItem() {
+    document.addEventListener('mousedown', (event) => {
+        let clickedElement = document.elementFromPoint(event.clientX, event.clientY);
+    
+        let script = document.createElement('script');
+        script.textContent = console.log(clickedElement); 
+        document.head.appendChild(script);
+
+        let validParent = _hasValidParent(clickedElement);
+
+        if (validClassNames.includes(clickedElement.className) 
+        || validIdNames.includes(clickedElement.id)
+        || validParent) {
+            chrome.runtime.sendMessage("create_menu");
+        } else { 
+            chrome.runtime.sendMessage("remove_menu");
+        }
+    })
+}
+
+function _hasValidParent(clickedElement) {
+
+    let parent = clickedElement.parentElement;
+    while (parent) {
+        if (validClassNames.includes(parent.className) || validIdNames.includes(parent.id)) { 
+            return true;
+        } 
+        parent = parent.parentElement;
+    }
+    return false;
+}
+
+//------------------------------------------------------
+
+function main() {
+    addMouseDownForListItem();
+}
+
+main();
